c724a84777d3dce5c0aa6acb2b68e47fbe3c90ad
Julian David Castillo Poveda <juliancas123456-cpu>
2025-08-28 12:44:24 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        }
+        System.out.println("Hola clase");
+
+
+
+        System.out.println(" ESTAMOS APRENDIENDO");
+        System.out.println("1. GIT STATUS -ESTADO DE RAMA");
+        System.out.println("2. GIT ADD . - AGRAGAR ARCHIVO");
+        System.out.println("3. GIT COMMIT - M - GUARDAR LOCAL");
+        System.out.println("4. GIT PUSH ORIGIN - SUBIR CAMBIOS");
+        System.out.println("5. GIT CLONE - CLONAR PROYECTO");
+        System.out.println("6. GIT BRANCH - VER LAS RAMAS");
+        System.out.println("7. GIT FETCH - RAMAS REMOTA ");
+        System.out.println("8 GIT SWITCH -C - CREAR Y POSICIONARME SOBRE LA RAMA");
 
 }
